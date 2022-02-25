2dfce745bbd1a6dfb2905c40c0a933191a1ce88b
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-24 16:25:32 -0800

    commit tree 645f22ab0e0b9a0b014cfde9be047889d829dc3b

--- index2.js
+++ index2.js
@@ -2,19 +2,20 @@
 // // 1) should return a new array containing the first two drivers from the passed-in array
 // // returnLastTwoDrivers()
 // // 2) should return an array of the last two drivers
-const returnFirstTwoDrivers = function (arr) {
-  let resultArr = [];
-  resultArr.push(arr[0]);
-  resultArr.push(arr[1]);
-  return resultArr;
-};
+// const returnFirstTwoDrivers = function (arr) {
+//   let resultArr = [];
+//   //   console.log(arr[0]);
+//   resultArr.push(arr[0]);
+//   resultArr.push(arr[1]);
+//   return resultArr;
+// };
 // // let driversArr = ["Donald", "Kelly", "Cheri"];
 // // console.log(returnFirstTwoDrivers(driversArr));
 
 // /////////////////////----------------🔥🔥🔥🔥🔥🔥🔥 returnLastTwoDrivers 🔥🔥🔥🔥🔥🔥🔥-----------------/////////////////////
-const returnLastTwoDrivers = function (arr) {
-  return [arr[arr.length - 2], arr[arr.length - 1]];
-};
+// const returnLastTwoDrivers = function (arr) {
+//   return [arr[arr.length - 2], arr[arr.length - 1]];
+// };
 // // let driversArr = ["Donald", "Kelly", "Cheri", "Donna"];
 // // console.log(returnLastTwoDrivers(driversArr));
 
@@ -22,42 +23,23 @@ const returnLastTwoDrivers = function (arr) {
 // // 3) has the `returnFirstTwoDrivers` function to as its first element
 // // 4) has the `returnLastTwoDrivers` function to as its last element
 // // 5) allows us to invoke either function from the array
-let driversArr = ["Donald", "Kelly", "Cheri", "Donna"];
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-// console.log(selectingDrivers[0](driversArr));
-// const example = selectingDrivers[0];
-// console.log(example(driversArr));
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// //console.log({ selectingDrivers }); ///---this gives me:[[Function: returnFirstTwoDrivers], [Function: returnLastTwoDrivers]] ?????
 
 // /////////////////////----------------🔥🔥🔥🔥🔥🔥🔥 createFareMultiplier 🔥🔥🔥🔥🔥🔥🔥-----------------/////////////////////
 // // 6) returns a function
 // // 7) should multiply a given value using the created multiplier
-const createFareMultiplier = (multiplierNum) => {
-  return function (fare) {
-    return fare * multiplierNum;
-  };
-};
+// const
 
-// /////////////////////----------------🔥🔥🔥🔥🔥🔥🔥 fareDoubler 🔥🔥🔥🔥🔥🔥🔥-----------------/////////////////////
+// // fareDoubler()
 // // 8) is a function
 // // 9) doubles fares
-function fareDoubler(fare) {
-  const doubler = createFareMultiplier(2);
-  return doubler(fare);
-}
 
-console.log(fareDoubler(8));
-// /////////////////////----------------🔥🔥🔥🔥🔥🔥🔥 fareTripler 🔥🔥🔥🔥🔥🔥🔥-----------------/////////////////////
+// // fareTripler()
 // // 10) is a function
 // // 11) triples fares
-function fareTripler(fare) {
-  const tripler = createFareMultiplier(3);
-  return tripler(fare);
-}
 
-// /////////////////////----------------🔥🔥🔥🔥🔥🔥🔥 selectDifferentDrivers 🔥🔥🔥🔥🔥🔥🔥-----------------/////////////////////
+// // selectDifferentDrivers(arrayOfDrivers, function)
 // // 12) returns the first two drivers when passed returnFirstTwoDrivers() as the second argument
 // // 13) returns the last two drivers when passed returnLastTwoDrivers() as the second argument
-
-function selectDifferentDrivers(arrDrivers, returnFirstTwoDrivers) {
-  return returnFirstTwoDrivers(arrDrivers);
-}
